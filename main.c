00af5fb265b778e759ae2370d10d00f7d422138c
Hetang Mehta <hetangmehta03@gmail.com>
2026-01-08 15:03:26 -0700

    UPdated Main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "lab1_IO.h"
+#include "matrixgen.c"
+
+int main(int argc, char *argv[])
+{
+    // First step, generate the Matrix using matrixgen.c
+
+    // Then run lab1_IO.c to run the multiplication
+
+    return 0;
+}
